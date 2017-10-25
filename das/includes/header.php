b640920fbf506d030f1a1d973574de6df1173340
HarryTate <harryjamestate@gmail.com>
2017-10-25 23:43:36 +0100

    Fix mobile

--- das/includes/header.php
+++ das/includes/header.php
@@ -12,7 +12,7 @@
   <meta name="theme-color" content="#ffffff">
 
 
-
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="css/style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
